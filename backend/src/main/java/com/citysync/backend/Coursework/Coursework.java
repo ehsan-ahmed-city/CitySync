01f1505d9b870ed8aadef11639d850257cafbb43
Ehsan Ahmed <ehsan.ahmed@city.ac.uk>
2026-01-28 18:52:34 +0000

    Courwork entity
    details on a coursework of a module like weighting, submission date and name ofc

---
+++ backend/src/main/java/com/citysync/backend/Coursework/Coursework.java
@@ -0,0 +1,47 @@
+package com.citysync.backend.coursework;
+
+import com.citysync.backend.module.Module;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "coursework")
+public class Coursework {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //many coursework items can belong to one module like many to one relatsonship
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "module_id", nullable = false)
+    private Module module;
+
+    @Column(nullable = false)
+    private String title; //like "PDD submission"
+
+
+    @Column(nullable = false)
+    private LocalDate dueDate;  //submission date for planning
+
+    //weighting (0-100% of module) optional but helps students
+    private Integer weighting;
+
+    @Column(nullable = false)
+    private Instant createdAt = Instant.now();//when it was created
+
+    protected Coursework() {
+        // jpa needs a constructore with no args
+    }
+
+    public Coursework(Module module, String title, LocalDate dueDate, Integer weighting) {
+        this.module = module;
+        this.title = title;
+        this.dueDate = dueDate;
+        this.weighting = weighting;
+    }
+
+
+}
