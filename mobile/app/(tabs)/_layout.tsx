62e9a855c4b92a07af514167118328452a30af7e
Ehsan Ahmed <ehsan.ahmed@city.ac.uk>
2026-02-20 23:32:53 +0000

    calendar settings routed to layout

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -37,6 +37,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="chevron.left.forwardslash.chevron.right" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="calendarSettings"
+        options={{
+          title: "Cal Settings",
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
